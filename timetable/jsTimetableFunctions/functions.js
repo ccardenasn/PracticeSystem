dcb40d561f8c88edc13dc1cc1b4748fc437c621e
Christian <ccardenasn@ic.uach.cl>
2017-01-21 20:14:02 -0300

    Avance de Horario

---
+++ timetable/jsTimetableFunctions/functions.js
@@ -0,0 +1,11 @@
+var condicion="0";
+
+function mostrar() {
+	div = document.getElementById('form_div');
+	div.style.display = '';
+}
+
+function cerrar(){
+	div = document.getElementById('form_div');
+	div.style.display = 'none';
+}
