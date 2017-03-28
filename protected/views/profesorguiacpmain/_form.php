27abe0fefd2fe9dc8e1a7594f07d2964240075a6
Christian <ccardenasn@ic.uach.cl>
2017-03-28 02:54:33 -0400

    Validacion de tabular input arreglada

--- protected/views/profesorguiacpmain/_form.php
+++ protected/views/profesorguiacpmain/_form.php
@@ -6,6 +6,7 @@
 $base = Yii::app()->baseUrl; 
 $js = Yii::app()->getClientScript();
 $js->registerScriptFile($base.'/tabularInputControls/tabularInputFunctions.js');
+$js->registerScriptFile($base.'/tabularInputControls/validateTabularFunctions.js');
 ?>
 
 <div class="form">
@@ -28,23 +29,32 @@ $js->registerScriptFile($base.'/tabularInputControls/tabularInputFunctions.js');
 	<table id="employee_table" align=center>
 		<tr id="row1">
 			<td>
-				<input type="text" name="RutProfGuiaCP[]" size="14" placeholder="Rut">
-				<input type="text" name="MailProfGuiaCP[]" size="14" placeholder="Mail">
+				<input type="text" id="Rut1" name="RutProfGuiaCP[]" size="14" placeholder="Rut" onblur="validateRut('Rut1');">
+				<br><span class='error_text' id='Rut1_error'></span>
+				<input type="text" id="Mail1" name="MailProfGuiaCP[]" size="14" placeholder="Mail" onblur="check_email('Mail1');">
+				<br><span class='error_text' id='Mail1_error'></span>
 			</td>
 			<td>
-				<input type="text" name="NombreProfGuiaCP[]" size="14" placeholder="Nombre">
-				<input type="text" name="TelefonoProfGuiaCP[]" size="14" placeholder="Telefono">
+				<input type="text" id="Nombre1" name="NombreProfGuiaCP[]" size="14" placeholder="Nombre" onblur="validateName('Nombre1');" >
+				<br><span class='error_text' id='Nombre1_error'></span>
+				<input type="text" id="Telefono1" name="TelefonoProfGuiaCP[]" size="14" placeholder="Telefono" onblur="validateTelefono('Telefono1');">
+				<br><span class='error_text' id='Telefono1_error'></span>
 			</td>
 			<td>
-				<input type="text" name="CursoProfGuiaCP[]" size="14" placeholder="Curso">
-				<input type="text" name="CelularProfGuiaCP[]" size="14" placeholder="Celular">
+				<input type="text" id="Curso1" name="CursoProfGuiaCP[]" size="14" placeholder="Curso">
+				<br><span class='error_text' id='Curso1_error'></span>
+				<input type="text" id="Celular1" name="CelularProfGuiaCP[]" size="14" placeholder="Celular" onblur="validateCelular('Celular1');">
+				<br><span class='error_text' id='Celular1_error'></span>
 			</td>
 			<td>
-				<input type="text" name="ProfesorJefeProfGuiaCP[]" size="14" placeholder="Profesor Jefe">
-				<input type="text" name="CentroPractica_RBD[]" size="14" placeholder="Centro de Práctica">
+				<input type="text" id="ProfesorJefe1" name="ProfesorJefeProfGuiaCP[]" size="14" placeholder="Profesor Jefe" onblur="check_fname('ProfesorJefe1');">
+				<br><span class='error_text' id='ProfesorJefe1_error'></span>
+				<input type="text" id="Centro1" name="CentroPractica_RBD[]" size="14" placeholder="Centro de Práctica" onblur="check_fname('Centro1');">
+				<br><span class='error_text' id='Centro1_error'></span>
 			</td>
 			<td>
-				<input type="text" name="ImagenProfGuiaCP[]" size="14" placeholder="Imagen">
+				<input type="text" id="Imagen1" name="ImagenProfGuiaCP[]" size="14" placeholder="Imagen" onblur="check_fname('Imagen1');">
+				<br><span class='error_text' id='Imagen1_error'></span>
 			</td>
 		</tr>
 	</table>
