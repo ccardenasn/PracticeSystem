da961c79e89e325f3b0da9e3f90998d5af977dd4
Christian <ccardenasn@ic.uach.cl>
2017-07-31 21:58:15 -0400

    redireccion de errores

--- protected/views/site/errorForbidden.php
+++ protected/views/site/errorForbidden.php
@@ -3,16 +3,14 @@
 /* @var $error array */
 
 $this->pageTitle=Yii::app()->name . ' - Error';
-$this->breadcrumbs=array(
+
+/*$this->breadcrumbs=array(
 	'Error',
-);
+);*/
 ?>
 
-<h2>¡Advertencia!<?php //echo $code; ?></h2>
+<h2>Error AGGGGG</h2>
 
 <div class="error">
-    <ul>
-        <li>Operación no permitida.</li>
-    </ul>
 <?php //echo CHtml::encode($message); ?>
 </div>
